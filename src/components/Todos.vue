13de25c0de1852a38164ac6f39e3cf81f6459d4c
shyamkumar yadav <shyamkumaryadav2003@gmail.com>
2020-08-13 17:52:52 +0530

    acceptmail.net

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,44 +1,48 @@
 <template>
-	<div>
-		<div :key="item.id" v-for="item in itmes" >
-			<Todo :item="item" />
-			<h1>This is Todo Elements</h1>
-		</div>
-	</div>
+  <div>
+    <v-alert type="success">I'm a success alert.</v-alert>
+
+    <v-alert type="info">I'm an info alert.</v-alert>
+
+    <v-alert type="warning">I'm a warning alert.</v-alert>
+
+    <v-alert type="error">I'm an error alert.</v-alert>
+  </div>
 </template>
 
 <script>
-import Todo from './Todo'
+// import Todo from "./Todo";
+import 
 
-export default{
-	name:"Todos",
-	// props:['itmes'],
-	components:{
-		Todo
-	},
-	data(){
+export default {
+  name: "Todos",
+  // props:['itmes'],
+  // components: {
+  //   Todo,
+  // },
+  data() {
     return {
       itmes: [
         {
-          id:1,
+          id: 1,
           name: "shyam",
-          is_done: true
+          is_done: true,
         },
         {
-          id:2,
+          id: 2,
           name: "shyam kumar yadav",
-          is_done: false
+          is_done: false,
         },
         {
-          id:2,
+          id: 2,
           name: "asasasasasas",
-          is_done: true
-        }
-      ]
-    }}
-}
+          is_done: true,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
