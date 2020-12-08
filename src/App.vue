6a93f1156cc25da80c16368013ff7f8b4f7c0ec4
shyamkumaryadav <shyamkumaryadav2003@gmail.com>
2020-12-09 01:10:46 +0530

    try new but not work

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <w-app>
+    <w-toolbar absolute>
+      <w-button icon="mdi mdi-dots" @click="isOpen = true"></w-button>
+      <div class="title2">Shyamkumar</div>
+      <div class="spacer"></div>
+      <span class="ml2"><w-icon>mdi mdi-home</w-icon></span>
+      <span class="ml2">{{ isOpen }}</span>
+      <span class="ml2">Item 3</span>
+    </w-toolbar>
+    <w-drawer 
+      v-model="isOpen" 
+      content-class="d-block">
+      <w-button
+        @click="isOpen = false"
+        md
+        outline
+        round
+        absolute
+        color="primary"
+        icon="wi-cross">
+      </w-button>
+    </w-drawer>
+    <router-view/>
+  </w-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      isOpen: false,
+      items1: [
+        { label: 'Item 1' },
+        { label: 'Item 2' },
+        { label: 'Item 3' },
+        { label: 'Item 4' }
+      ]
+    }
+  }
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import WaveUI from 'wave-ui'
+import 'wave-ui/dist/wave-ui.css'
+import '@mdi/font/css/materialdesignicons.min.css'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp({
+    render: h => h(App)
+}).use(store).use(router)
+
+new WaveUI(app, {
+    // Some Wave UI options.
+})
+
+app.mount('#app')
